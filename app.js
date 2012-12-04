56338038c3f374aa7bd7e2a0e0f35a00c167740d
justinjaywang <justinjaywang@gmail.com>
2012-12-04 14:12:05 -0800

    added font-awesome icons

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use('/css/', express.static(__dirname + '/css'));
 app.use('/js/', express.static(__dirname + '/js'));
 app.use('/images/', express.static(__dirname + '/images'));
 app.use('/touch-icons/', express.static(__dirname + '/touch-icons'));
+app.use('/font/', express.static(__dirname + '/font'));
 
 /* functions */
 function formatTime(timeString) {
