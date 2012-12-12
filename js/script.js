e33cbe6f2e286246dba8a5d591b6efe3295fdc71
96chan <chan.berkeley@gmail.com>
2012-12-12 09:42:03 -0800

    modified modal function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 $('.remind').live('click',function(){
+	$('body').addClass('modalOpen');
+	$('#modal-hide').hide();
 	var msg ="Do you want to email "+$(this).attr("id") + "?";
+	$('#modal').html('<div class="content-padded-custom content-modal"><p class="settle-paragraph">'+msg+'</p><a class="button-positive button-block modal-button" data-ignore="push">Remind</a>    <a class="modalOpener button button-block modal-button">Cancel</a></div></div><script>$(".modal-button").live("click",function(){	$("body").removeClass("modalOpen");		$("#modal-hide").show();	$("#modal").html("");});</script>');
 	console.log(msg);
-	new Messi(msg, {buttons: [{id: 0, label: 'Yes', val: 'Y'}, {id: 1, label: 'No', val: 'N'}], modal:true});
 });
 $('.settle').live('click',function(){
+	$('body').addClass('modalOpen');
+	$('#modal-hide').hide();
 	var msg ="Already settled with "+$(this).attr("id") + "?";
+	$('#modal').html('<div class="content-padded-custom content-modal"><p class="settle-paragraph">'+msg+'</p><a class="button-positive button-block modal-button" data-ignore="push">Settle up</a>    <a class="modalOpener button button-block modal-button">Cancel</a></div></div><script>$(".modal-button").live("click",function(){	$("body").removeClass("modalOpen");		$("#modal-hide").show();	$("#modal").html("");});</script>');
 	console.log(msg);
-	new Messi(msg, {buttons: [{id: 0, label: 'Yes', val: 'Y'}, {id: 1, label: 'No', val: 'N'}], modal:true});
 });
