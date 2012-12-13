bdcb978625003f784cea69cd35ef92bdc965f1b1
96chan <chan.berkeley@gmail.com>
2012-12-12 18:11:16 -0800

    modifying modal background color and modal of the delete button

--- js/script.js
+++ js/script.js
@@ -12,3 +12,17 @@ $('.settle').live('click',function(){
 	$('#modal').html('<div class="content-padded-custom content-modal"><p class="settle-paragraph">'+msg+'</p><a class="button-positive button-block modal-button" data-ignore="push">Settle up</a>    <a class="modalOpener button button-block modal-button">Cancel</a></div></div><script>$(".modal-button").live("click",function(){	$("body").removeClass("modalOpen");		$("#modal-hide").show();	$("#modal").html("");});</script>');
 	console.log(msg);
 });
+$('.delete').live('click',function(){
+	$('body').addClass('modalOpen');
+	$('#modal-hide').hide();
+	var msg ="Are you sure you want to delete the group  "+$(this).attr("id") + "?";
+	$('#modal').html('<div class="content-padded-custom content-modal"><p class="settle-paragraph">'+msg+'</p><a class="button-negative button-block modal-button" data-ignore="push">Delete</a>    <a class="modalOpener button button-block modal-button">Cancel</a></div></div><script>$(".modal-button").live("click",function(){	$("body").removeClass("modalOpen");		$("#modal-hide").show();	$("#modal").html("");});</script>');
+	console.log(msg);
+});
+$('.delete-payment').live('click',function(){
+	$('body').addClass('modalOpen');
+	$('#modal-hide').hide();
+	var msg ="Are you sure you want to delete this payment?";
+	$('#modal').html('<div class="content-padded-custom content-modal"><p class="settle-paragraph">'+msg+'</p><a class="button-negative button-block modal-button" data-ignore="push">Delete</a>    <a class="modalOpener button button-block modal-button">Cancel</a></div></div><script>$(".modal-button").live("click",function(){	$("body").removeClass("modalOpen");		$("#modal-hide").show();	$("#modal").html("");});</script>');
+	console.log(msg);
+});
